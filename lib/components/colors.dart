e6c8d6f62881dcdfcb13043416d82202012bf6bd
scientistsagoemarcus <Kwakuabeikusagoe@gmail.com>
2022-01-07 12:42:21 +0000

    commit to master

---
+++ lib/components/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color backgroundcolor = const Color(0XFF355C7D);
+  static Color foregroundcolor = const Color(0XFF6C5B7B);
+  static Color maincolor = const Color(0XFFF8B195);
+  static Color contcolor = const Color(0XFFFF9D58);
+  static Color circolor = const Color(0XFF45ADA8);
+  static Color container = const Color(0XFF918E8E);
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'components/colors.dart';
+
+void main() => runApp(const BusCard());
+
+class BusCard extends StatelessWidget {
+  const BusCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: AppColors.circolor,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('images/user.jpeg'),
+                ),
+                const Text(
+                  'Bright Marcus Sagoe',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Pacifico',
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.5),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 30,
+                  width: 230,
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 6),
+                      Icon(
+                        Icons.call,
+                        color: AppColors.circolor,
+                        size: 22,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text('+233554741410',
+                          style: TextStyle(color: AppColors.circolor))
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                Container(
+                  height: 30,
+                  width: 230,
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Icon(
+                        Icons.mail,
+                        color: AppColors.circolor,
+                        size: 22,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text('Kwakuabeikusagoe@gmail.com',
+                          style: TextStyle(color: AppColors.circolor))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
